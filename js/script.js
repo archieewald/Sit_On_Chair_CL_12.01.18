700ef7653a03e9ddc4a4c6d961e43df63aa6e890
Archieewald <Arkadiuszdabek@gmail.com>
2018-01-31 22:33:34 +0100

    Warsztat Sit On Chair 80%

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var slides = document.querySelectorAll(".slider-content");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "flex";
+}
